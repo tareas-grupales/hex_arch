d3c31b036cada2cea8b6f6d17cd959c4cc608dcf
Alex Vega <axvg@users.noreply.github.com>
2025-12-01 20:14:19 -0500

    update domain

--- src/main/java/com/library/gestion_prestamos/domain/ports/in/GetBorrowedBooksUseCase.java
+++
@@ -1,9 +0,0 @@
-package com.library.gestion_prestamos.domain.ports.in;
-
-import java.util.List;
-
-import com.library.gestion_prestamos.domain.model.Book;
-
-public interface GetBorrowedBooksUseCase {
-    List<Book> getAllBorrowed();
-}
--- src/main/java/com/library/gestion_prestamos/domain/ports/in/UpdateBookUseCase.java
+++
@@ -1,5 +0,0 @@
-package com.library.gestion_prestamos.domain.ports.in;
-
-public interface UpdateBookUseCase {
-    void updateBookAvailability(Long bookId, Boolean disponible);
-}
